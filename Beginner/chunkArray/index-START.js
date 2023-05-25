680380de5c0e5cc3827dddacc5b9bdcd53bdc851
nick <108027865+nickaldwin@users.noreply.github.com>
2023-05-25 14:48:51 +0800

    Update index-START.js

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -6,7 +6,48 @@
 
 function chunkArray(array, size) {
     // Code goes here
+ let result = [];
+ 
+ for(value of array){
+  let lastArray = result[result.length - 1]
+  if(!lastArray || lastArray.length == size){
+    result.push[(value)]
+  }else{
+   lastArray.push(value)  
+  }
+ }
+ return result
 }
+
+//Looping through number of chunks
+ function chunkArray(array, size){
+    let result = [];
+  let arrayCopy = [...array]
+  
+  while (arrayCopy.length > 0){
+   result.push(arrayCopy.slice(0, size))
+  }
+  return result
+ } 
+
+//using .slice method
+function chunckArray(array, size){
+ let result = [];
+   for(let i = 0; i<array.lengh; i=+ size){
+    let chunk = array.slice(i, i + size)
+    result.push(chunk)
+   }
+ return result
+}
+ 
+//Recusion
+function chunkArray(array, size){
+  if(array.length <= size){
+    return [array]
+  }
+ return [array.slice(0, size), ...chunkArray(array.slice(size), size)]
+}
+
 
 
 module.exports = chunkArray
