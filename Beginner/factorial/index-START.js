e0011b714557ddc05021a7532fef472997534c21
nick <108027865+nickaldwin@users.noreply.github.com>
2023-05-25 15:13:12 +0800

    Update index-START.js

--- Beginner/factorial/index-START.js
+++ Beginner/factorial/index-START.js
@@ -3,8 +3,44 @@ Write a function that returns the factorial of
 the provided integer(n). E.g
     factorial(5) // should return 120
 */
+// using for loop
+function factiorial(n){
+  if(n <= 1){
+      return 1
+  }
+    for(let i = n -1; i >= 1 ; i--){
+        n *=1;
+    }
+    return n
+}
+
+//using while loop
+function factorial(n){
+    let result = n 
+    if(n <= 1){
+        return 1
+    }
+    while (n > 1){
+        n--
+        result *= n
+        
+    }
+    return result
+}
+
+//using recursion
+function factiorial(n){
+    if(n === 0 ){
+        return 1
+    }
+    return n * factiorial(n -1)
+}
 
 
+
+ 
+
+//using MEMOIZATION
 function factorial(n, memo) {
     memo = memo || {}
     if(memo[n]){
