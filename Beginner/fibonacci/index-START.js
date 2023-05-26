fdd671cd6f15964601d9fddd3c1b2aea6b807692
nick <108027865+nickaldwin@users.noreply.github.com>
2023-05-26 13:32:08 +0800

    Update index-START.js

--- Beginner/fibonacci/index-START.js
+++ Beginner/fibonacci/index-START.js
@@ -7,6 +7,43 @@ where the sequence is:
 
 function fibonacci(n) {
     // Code goes here
+    let previous = 1,
+        current = 1
+    if(n <= 1){
+        return 1
+    }else{
+        let counter = - 1
+            // lets do the while loop
+        while(counter){
+            let tmp = curent
+            current += previous
+            previous = tmp
+                counter--
+        }
+    }
+    return current
 }
+// a recursive solution
+function fibonacci(n){
+    if(<= 1){
+        return 1
+    }
+    return  fibonacci( n - 1) + fibonacci(n -2) 
+}
+
+// using memo
+
+function fibonacci(n, memo){
+    memo = memo || {}
+    if(memo[n]){
+        return memo[n]
+    }
+    if (n <= 1){
+        return 1
+    }
+    return memo[n] = fiboacci(n - 1, memo)+ fiboacci(n - 2, memo) 
+}
+
+
 
 module.exports = fibonacci
