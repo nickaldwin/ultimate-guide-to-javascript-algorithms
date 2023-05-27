a859c9645db4e997deb1a1238946ea0c786ac3ff
nick <108027865+nickaldwin@users.noreply.github.com>
2023-05-27 14:53:07 +0800

    Update index-START.js

--- Beginner/fizzBuzz/index-START.js
+++ Beginner/fizzBuzz/index-START.js
@@ -8,8 +8,32 @@
 
 
 function fizzBuzz(n) {
-    // Code goes here
+      for(let i = 1; i <= n; i++){
+            //is multiple by 3 and 5
+          if(i % 15 === 0){
+            console.log('fizzbuzz')
+          }else if(i % 3 === 0){
+                // is multiple of 3
+              console.log('fizz')
+          }else if(i % 5 === 0){
+              //multiple by 5 
+              console.log('buzz')
+          }else{
+            //is nether above 
+              console.log(i)
+          }
+      }
 }
+
+
+//cleaner & smaller
+//crete a function
+function fizBuzz(n){
+    for(let i = 0; i < n; i++)
+        console.log((++i % 3 ? '' : 'Fizz' ) + (i % 5 ? '' : 'bizz') || i)
+}
+
+
 
 
 module.exports = fizzBuzz
